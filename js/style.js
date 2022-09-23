bb2ac38b0444b9b9e88b5943ca6fd0e32366106e
citrachantikasaadah <citrachantikasaadah@gmail.com>
2022-09-23 08:58:27 +0700

    index.html
    start

--- js/style.js
+++ js/style.js
@@ -2,6 +2,7 @@ let menu = document.querySelector('#menu-bars');
 let header = document.querySelector('header');
 
 menu.onclick = () =>{
+    // menu.classList.toggle('fa-times');
     header.classList.toggle('active');
 }
 
